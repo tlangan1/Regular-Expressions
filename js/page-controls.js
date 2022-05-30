9e8afa5c8993ed95aff7f9f9615af3c3a5763070
Thomas Langan <tom.langan@comcast.net>
2022-05-30 12:51:00 -0400

    Add miscellaneous examples page. Remove test strings leaving only the multi-line string. Wrap error handling around the replaceAll call. Rearrange order of experiment controls. Further document regular expressions. Remove "global match" option...apply it to all matches.

--- js/page-controls.js
+++ js/page-controls.js
@@ -36,12 +36,48 @@ function onLoad(event) {
     .addEventListener("click", toggleExperiment);
 
   document
+    .querySelector("button.examples-page")
+    .addEventListener("click", gotoExamples);
+
+  document
     .querySelector("#non-word-boundary-example-1")
     .addEventListener("click", nonWordBoundaryExample("\\Bad"));
 
   document
     .querySelector("#non-word-boundary-example-2")
     .addEventListener("click", nonWordBoundaryExample("ad\\B"));
+
+  document
+    .querySelector("#dot-all-example-1")
+    .addEventListener("click", dotAllExample(".", ""));
+
+  document
+    .querySelector("#dot-all-example-2")
+    .addEventListener("click", dotAllExample(".", "s"));
+
+  document
+    .querySelector("#capitalized-words")
+    .addEventListener("click", miscellaneousExample(1));
+
+  document
+    .querySelector("#capitalized-sentances")
+    .addEventListener("click", miscellaneousExample(2));
+
+  document
+    .querySelector("#uncapitalized-sentances")
+    .addEventListener("click", miscellaneousExample(3));
+
+  document
+    .querySelector("#domain-names")
+    .addEventListener("click", miscellaneousExample(4));
+
+  document
+    .querySelector("#case-insensitive-match")
+    .addEventListener("click", ApplyRegExp);
+
+  document
+    .querySelector("#dot-all-match")
+    .addEventListener("click", ApplyRegExp);
 
   // --------------------------------------------------
   // The helper functions for the executable code above
@@ -59,6 +95,7 @@ function onLoad(event) {
     document
       .querySelector("#p" + currentPageNumber)
       .classList.remove("visible");
+
     currentPageNumber -= 1;
     document.querySelector("#p" + currentPageNumber).classList.add("visible");
 
@@ -74,9 +111,9 @@ function onLoad(event) {
     document
       .querySelector("#p" + currentPageNumber)
       .classList.remove("visible");
+
     currentPageNumber += 1;
     document.querySelector("#p" + currentPageNumber).classList.add("visible");
-    "#p" + (currentPageNumber - 1);
 
     document.querySelector(".previous-page").attributes[
       "more_pages"
@@ -86,6 +123,21 @@ function onLoad(event) {
         "more_pages"
       ].value = false;
     }
+  }
+
+  function gotoExamples(event) {
+    document
+      .querySelector("#p" + currentPageNumber)
+      .classList.remove("visible");
+
+    currentPageNumber = document.querySelectorAll(".page").length;
+    "#p" + (currentPageNumber - 1);
+    document.querySelector("#p" + currentPageNumber).classList.add("visible");
+
+    document.querySelector(".previous-page").attributes[
+      "more_pages"
+    ].value = true;
+    document.querySelector(".next-page").attributes["more_pages"].value = false;
   }
 
   function returnToggleExperiment() {
@@ -133,16 +185,79 @@ function onLoad(event) {
 
   function nonWordBoundaryExample(pattern) {
     function specificNonWordBoundaryExample() {
-      document.querySelector("#test-string-1").value = "bad";
-      document.querySelector("#test-string-2").value = "advice";
-      document.querySelector(".pattern").value = pattern;
+      clearTestString();
+      document.querySelector("#test-multi-line-string").value =
+        "The frog got some really bad advice from the badger!";
+      document.querySelector("#pattern").value = pattern;
       // I need to close over the same function object, toggleExperiment, here and in the addEventListener above
       // however, I need to override the toggle.  Regardless of which state it is in I need is to become visible.
       toggleExperiment(event, true);
 
-      ApplyRegExp(document.querySelector(".pattern").value);
+      ApplyRegExp();
     }
 
     return specificNonWordBoundaryExample;
+  }
+
+  function dotAllExample(pattern, dotAllFlag) {
+    function specificDotAllExample() {
+      clearTestString();
+      document.querySelector("#test-multi-line-string").value =
+        "This line is terminated\nbefore the sentance ends";
+      document.querySelector("#pattern").value = pattern;
+      if (dotAllFlag == "s")
+        document.querySelector("#dot-all-match").checked = true;
+      else document.querySelector("#dot-all-match").checked = false;
+      // I need to close over the same function object, toggleExperiment, here and in the addEventListener above
+      // however, I need to override the toggle.  Regardless of which state it is in I need is to become visible.
+      toggleExperiment(event, true);
+
+      ApplyRegExp();
+    }
+
+    return specificDotAllExample;
+  }
+
+  function miscellaneousExample(number) {
+    var miscellaneousExamples = [
+      // identify all capitalized words
+      {
+        string: "The dog is home. maybe baby. Or is he? He is!",
+        pattern: "[A-Z]\\w+",
+      },
+      // identify all sentances that are properly capitalized
+      {
+        string: "The dog is home. maybe baby. Or is he? He is!",
+        pattern: "[A-Z][^.!?]*[.!?]",
+      },
+      {
+        // identify sentances which are not capitalized.
+        string: "the dog is home. Maybe baby. or is he? He is!",
+        pattern: "((?<=[.!?]\\s+)[a-z]\\w+|^[a-z]\\w+)[^.!?]*[.!?]",
+      },
+      {
+        string: "site.com my.site.com frog again.my.site.com",
+        pattern: "(\\w+\\.)+\\w+",
+      },
+    ];
+
+    function example() {
+      clearTestString();
+      document.querySelector("#test-multi-line-string").value =
+        miscellaneousExamples[number - 1].string;
+      document.querySelector("#pattern").value =
+        miscellaneousExamples[number - 1].pattern;
+      // I need to close over the same function object, toggleExperiment, here and in the addEventListener above
+      // however, I need to override the toggle.  Regardless of which state it is in I need is to become visible.
+      toggleExperiment(event, true);
+
+      ApplyRegExp();
+    }
+
+    return example;
+  }
+
+  function clearTestString() {
+    document.querySelector("#test-multi-line-string").value = "";
   }
 }
--- js/reg-exp.js
+++ js/reg-exp.js
@@ -1,30 +1,41 @@
-function ApplyRegExp(exp) {
-  var modifiers = "";
-  if (document.querySelector("#global-match").checked) {
-    modifiers += "g";
-  }
+function ApplyRegExp() {
+  var exp = document.querySelector("#pattern").value;
+
+  // When using replaceAll with a regex the global modifier must be specified...it is not optional.
+  var modifiers = "g";
+  // Hence, I removed the "#global-match" checkbox.
+  // if (document.querySelector("#global-match").checked) {
+  //   modifiers += "g";
+  // }
   if (document.querySelector("#case-insensitive-match").checked) {
     modifiers += "i";
+  }
+  if (document.querySelector("#dot-all-match").checked) {
+    modifiers += "s";
   }
   if (document.querySelector("#test-multi-line-string")) {
     modifiers += "m";
   }
 
-  exp = RegExp(exp, modifiers);
+  document.querySelector(".error").innerHTML = "";
+  try {
+    exp = RegExp(exp, modifiers);
+  } catch (error) {
+    document.querySelector(".error").innerHTML = error;
+    return;
+  }
 
-  if (document.querySelector("#test-string-1")) {
-    document.querySelector("#test-result-1").innerText = document
-      .querySelector("#test-string-1")
-      .value.match(exp);
-  }
-  if (document.querySelector("#test-string-2")) {
-    document.querySelector("#test-result-2").innerText = document
-      .querySelector("#test-string-2")
-      .value.match(exp);
-  }
   if (document.querySelector("#test-multi-line-string")) {
-    document.querySelector("#test-multi-line-result").innerText = document
+    document.querySelector("#test-multi-line-result").innerHTML = document
       .querySelector("#test-multi-line-string")
-      .value.match(exp);
+      .value.replaceAll(exp, replacerFunction);
+  }
+
+  /*--------------------------------------------------*/
+  /* functions to support the executable code above   */
+  /*--------------------------------------------------*/
+  // Wrap each item in the target string with spans
+  function replacerFunction(item) {
+    return "<span>" + item + "</span>";
   }
 }
